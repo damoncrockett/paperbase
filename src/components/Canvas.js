77875acfade2524900c85e5fe71b083390612e67
damoncrockett <damoncrockett@hotmail.com>
2022-02-22 16:12:13 -0500

    three hello world
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as THREE from 'three';
 
 class Canvas extends Component {
   constructor(props) {
@@ -30,7 +31,62 @@ class Canvas extends Component {
 
 
   drawCanvas() {
+    // Create the scene and a camera to view it
+    var scene = new THREE.Scene();
 
+    // Specify the portion of the scene visiable at any time (in degrees)
+    var fieldOfView = 75;
+
+    // Specify the camera's aspect ratio
+    var aspectRatio = window.innerWidth / window.innerHeight;
+
+    // Specify the near and far clipping planes. Only objects
+    // between those planes will be rendered in the scene
+    // (these values help control the number of items rendered
+    // at any given time)
+    var nearPlane = 0.1;
+    var farPlane = 1000;
+
+    // Use the values specified above to create a camera
+    var camera = new THREE.PerspectiveCamera(
+      fieldOfView, aspectRatio, nearPlane, farPlane
+    );
+
+    // Finally, set the camera's position in the z-dimension
+    camera.position.z = 5;
+
+    // Create the canvas with a renderer and tell the
+    // renderer to clean up jagged aliased lines
+    var renderer = new THREE.WebGLRenderer({antialias: true});
+
+    // Specify the size of the canvas
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    // Add the canvas to the DOM
+    document.body.appendChild( renderer.domElement );
+
+    // Create a cube with width, height, and depth set to 1
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+
+    // Use a simple material with a specified hex color
+    var material = new THREE.MeshBasicMaterial({ color: 0x1e90ff });
+
+    // Combine the geometry and material into a mesh
+    var cube = new THREE.Mesh( geometry, material );
+
+    // Add the mesh to the scene
+    scene.add( cube );
+
+    function animate() {
+      requestAnimationFrame( animate );
+      renderer.render( scene, camera );
+
+      // Rotate the object a bit each animation frame
+      cube.rotation.y += 0.01;
+      cube.rotation.z += 0.01;
+    }
+
+    animate();
 
   }
 
