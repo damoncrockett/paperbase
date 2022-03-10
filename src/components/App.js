98f549bc892fd730169e354ea3bb77b9640abeef
damoncrockett <damoncrockett@hotmail.com>
2022-03-10 18:03:25 -0500

    buffer geometry imperative update
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,52 @@
-import React, { Component } from "react";
-import Field from './Field';
+import React, { Component, useEffect, useRef, useState } from "react";
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { BufferAttribute } from 'three';
+
+function Scatter(props) {
+  const itemSize = 3;
+  const data = props.data;
+
+  const ref = useRef();
+
+  if (data !== null) {
+    const vertices = new Float32Array(data.pos);
+    const normals = new Float32Array(data.norm);
+
+    // this can apparently be useEffect or useLayoutEffect, no difference
+    useEffect(() => {
+        if (ref.current) {
+          const newVertices = new BufferAttribute( vertices, itemSize );
+          ref.current.geometry.setAttribute("position", newVertices);
+          ref.current.geometry.attributes.position.needsUpdate = true;
+        }
+      },
+      [vertices]
+    );
+
+    return (
+      <mesh ref={ref}>
+        <bufferGeometry>
+          <bufferAttribute
+            attachObject={['attributes', 'position']}
+            array={vertices}
+            count={vertices.length / itemSize}
+            itemSize={itemSize}
+          />
+          <bufferAttribute
+            attachObject={['attributes', 'normal']}
+            array={normals}
+            count={normals.length / itemSize}
+            itemSize={itemSize}
+          />
+        </bufferGeometry>
+        <meshPhongMaterial color={'dodgerblue'} />
+      </mesh>
+    )
+  } else {
+    return null
+  }
+}
 
 class App extends Component {
   constructor(props) {
@@ -36,7 +83,7 @@ class App extends Component {
     fetch(this.returnDomain()+this.state.model+'.json')
       .then(response => response.json())
       .then(data => this.setState(state => ({
-        data: data
+        data: data[0]
       })));
     }
 
@@ -48,23 +95,28 @@ class App extends Component {
     }
 
   render() {
-      return (
-        <div className='app'>
-          <div id='componentEnclosure'>
-            <Field
+    return (
+      <div className='app'>
+        <div id='componentEnclosure'>
+          <Canvas dpr={[1, 2]} camera={{ position: [1, 1, 4000], far: 100000 }}>
+            <color attach="background" args={[0xfff8dc]} />
+            <ambientLight />
+            <pointLight position={[1, 1, 2000]} />
+            <Scatter
               data={this.state.data}
-              model={this.state.model}
             />
-          </div>
-          <div className='buttonStrip'>
-            <div className='radSwitch' onChange={this.handleModel}>
-              <input type="radio" value={'pn'} name="Model" defaultChecked /> PCA
-              <input type="radio" value={'tn'} name="Model" /> t-SNE
-              <input type="radio" value={'un'} name="Model" /> UMAP
-            </div>
+            <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+          </Canvas>
+        </div>
+        <div className='buttonStrip'>
+          <div className='radSwitch' onChange={this.handleModel}>
+            <input type="radio" value={'pn'} name="Model" defaultChecked /> PCA
+            <input type="radio" value={'tn'} name="Model" /> t-SNE
+            <input type="radio" value={'un'} name="Model" /> UMAP
           </div>
         </div>
-    );
+      </div>
+    )
   }
 }
 
