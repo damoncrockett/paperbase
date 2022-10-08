9fb164c521e82a1b5e94bc3bb0fc85b651e403b9
damoncrockett <damoncrockett@hotmail.com>
2022-10-08 00:18:50 -0400

    4x4 tile

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -20,22 +20,26 @@ export default function Landing({ setPage }) {
         <button onClick={() => setPage('app')}>Explore the collection</button>
       </div>
       <div id='canvasContainer'>
-        <Canvas>
+        <Canvas camera={{ fov: 75, position: [0, 0, 10] }}>
           <ambientLight intensity={0.5} />
           <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
           <pointLight position={[-10, -10, -10]} />
-          <Box position={[2.5, 0, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
-          <Box position={[7.5, 0, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
-          <Box position={[-2.5, 0, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
-          <Box position={[-7.5, 0, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
-          <Box position={[2.5, 5, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
-          <Box position={[7.5, 5, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
-          <Box position={[-2.5, 5, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
-          <Box position={[-7.5, 5, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
-          <Box position={[2.5, -5, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
-          <Box position={[7.5, -5, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
-          <Box position={[-2.5, -5, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
-          <Box position={[-7.5, -5, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]}/>
+          <Box position={[2.5, 8, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[7.5, 8, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[-2.5, 8, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[-7.5, 8, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[2.5, 3, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[7.5, 3, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[-2.5, 3, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[-7.5, 3, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[2.5, -2, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[7.5, -2, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[-2.5, -2, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[-7.5, -2, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[2.5, -7, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[7.5, -7, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[-2.5, -7, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
+          <Box position={[-7.5, -7, -8]} rotation={[Math.PI * 2 * Math.random(), 0, 0]} color={0x4b2f3b} isrot={true}/>
         </Canvas>
       </div>
       <div id='landingFooter'>
@@ -45,17 +49,21 @@ export default function Landing({ setPage }) {
   )
 }
 
-function Box(props) {
+function Box({ position, rotation, color, isrot }) {
   const ref = useRef()
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
+
+  if ( isrot ) {
+    useFrame((state, delta) => (ref.current.rotation.x += delta ))
+  }
 
   return (
     <mesh
-      {...props}
+      position={position}
+      rotation={rotation}
       ref={ref}
       scale={4}>
       <boxGeometry args={[1, 1, 0.25]} />
-      <meshLambertMaterial color={0x4b2f3b}/>
+      <meshLambertMaterial color={color}/>
     </mesh>
   )
 }
